fe17432c80e3b1b7894d1d8730419b6edbe6e1a0
cmsbuild <>
2008-12-08 23:09:38 +0000

    This commit was manufactured by cvs2git to create tag 'DQMOffline- Configuration-V00-01-21'.
    Sprout from master 2008-12-08 23:09:37 UTC Giuseppe Della Ricca <sha1-2ac9df825d52a0270fbcfdafbd6206ce1b324729@cern.ch> ''
    Cherrypick from master 2008-08-14 09:24:51 UTC Stefano Argiro <sha1-27144880fec262534c9bc969ae330367d420f8b3@cern.ch> 'added pi0 calib':
        python/ALCARECOEcalCalDQM_cff.py

--- python/ALCARECOEcalCalDQM_cff.py
+++ python/ALCARECOEcalCalDQM_cff.py
@@ -2,10 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 import DQMOffline.CalibCalo.MonitorAlCaEcalPhisym_cfi
 import DQMOffline.CalibCalo.MonitorAlCaEcalPi0_cfi
-import DQMOffline.CalibCalo.MonitorAlCaEcalEleCalib_cfi
 
 ALCARECOEcalCalPhisymDQM = DQMOffline.CalibCalo.MonitorAlCaEcalPhisym_cfi.EcalPhiSymMon.clone()
 
 ALCARECOEcalCalPi0CalibDQM =  DQMOffline.CalibCalo.MonitorAlCaEcalPi0_cfi.EcalPi0Mon.clone()
-
-ALCARECOEcalCalElectronCalibDQM =  DQMOffline.CalibCalo.MonitorAlCaEcalEleCalib_cfi.EcalEleCalibMon.clone()
